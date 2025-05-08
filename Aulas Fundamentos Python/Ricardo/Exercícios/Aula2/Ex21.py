6edcd535b28cd577861360611ce01b3fb252adfd
João Mendes <mendes19966@gmail.com>
2025-05-08 11:39:08 +0100

    Updates

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula2/Ex21.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula2/Ex21.py
@@ -1,3 +1,6 @@
 frase = input("Escreva uma frase: ")
+leterACount = frase.count("A")
+aPositionFirst = frase.find("A")+1
+aPositionLast = frase.rfind("A")+1
 
-print(f"A letra A aparece: {frase.count("A")}, vezes na sua frase.")
+print(f"A letra A aparece: {leterACount}, vezes na sua frase.\nA posição que aparece na primeira vez é: {aPositionFirst}\n A posição que aparece por a ultima vez é: {aPositionLast}")
