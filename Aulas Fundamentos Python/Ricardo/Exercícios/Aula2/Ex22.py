deaff8eb53fed2330106445f412662975a5f9208
João Mendes <mendes19966@gmail.com>
2025-05-08 10:14:52 +0100

    Update Ex22.py

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula2/Ex22.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula2/Ex22.py
@@ -2,5 +2,3 @@ nomeCompleto = input("Escreva o nome: ")
 nomeMinusculas = nomeCompleto.lower()
 
 print(f"Olá {nomeMinusculas.split()[0]}.\nO seu email é {nomeMinusculas.split()[0]}@empresa.com")
-
-# Coment trial
