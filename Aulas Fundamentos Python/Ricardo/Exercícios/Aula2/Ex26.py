4ff5e1c4681edd9d581b928aa817f2ea4e762b07
João Mendes <mendes19966@gmail.com>
2025-05-12 10:05:06 +0100

    Update Ex26.py

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula2/Ex26.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula2/Ex26.py
@@ -1,17 +1,15 @@
 soma = 0
 validInputs = 0
 
-while validInputs < 5:
-    nota = int(input("Informe a nota do aluno: "))
-    if nota <= 0 or nota >= 20:
-        print("Numero inválido, insira outro valor.")
-    else:
-        soma = soma + nota
-        validInputs += 1
+nota1 = int(input("Informe a nota do aluno 1: "))
+nota2 = int(input("Informe a nota do aluno 2: "))
+nota3 = int(input("Informe a nota do aluno 3: "))
+nota4 = int(input("Informe a nota do aluno 4: "))
+nota5 = int(input("Informe a nota do aluno 5: "))
 
-media = soma / 5
+media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
 
-print(f"A média do aluno é {soma} a dividir por 5 = {media}")
+print(f"A média do aluno é: {media}")
 if media >= 9.5:
   print("Aprovado")
 elif media > 8 and media < 9.5:
