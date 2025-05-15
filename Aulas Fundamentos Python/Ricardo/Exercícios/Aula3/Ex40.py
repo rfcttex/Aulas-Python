66eee3c437df98e3c99580643f65280bb4cd58d1
João Mendes <mendes19966@gmail.com>
2025-05-15 12:23:32 +0100

    Update Ex40.py

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula3/Ex40.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula3/Ex40.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 dataAtual = datetime.now().year
 
-menores = False
+maior = 0
+menor = 0
 
 for i in range(0, 10, 1):  
-    anoNascimento = int(input("Insira o ano de nascimento da pessoa: "))
+    print(f"Insira o ano de nascimento da {i+1}º pessoa: ")
+    anoNascimento = int(input(""))
     idade = dataAtual - anoNascimento
     
-    
-    if idade >= 18:
-        maiores = maiores + 1
-    else:
-        menores = menores + 1 
+    if idade > maior:
+        maior = idade
+    elif idade == 0 or idade < maior : #? Logica
+        menor = idade 
 
-print(f"A menor idade é: {menores}")
+print(f"A de maior idade é {maior}\vA menor idade é {menor}")
