776e6fc9c29d0e2a0d4554206cad9b3912f3078d
João Mendes <mendes19966@gmail.com>
2025-05-22 09:42:09 +0100

    Update Ex58.py

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula6/Ex58.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula6/Ex58.py
@@ -1,12 +1,15 @@
 valores = []
 menor = None
 maior = None
+numCounter = 0
 
 for i in range(0,5,1):
-  numero = int(input("Deseja escrever um numero? (s/n): "))
+  numero = int(input("Escreva um numero: "))
   valores.append(numero)
-
-  if valores[i] < valores [i+1]:
-    valores.insert(0,numero)
+  numCounter += 1
+  if numCounter > 1:
+    if (numero) < (valores.index(i)):
+      valores.insert(0,numero)
+    pass
   
 print(f"Valores inseridos já inseridos: {valores}")
