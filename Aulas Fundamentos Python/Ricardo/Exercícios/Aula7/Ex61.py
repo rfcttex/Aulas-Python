665eb73e016fa13c0394c67666cc6ae9cf12a80b
João Mendes <mendes19966@gmail.com>
2025-05-23 19:05:49 +0100

    updates on 61 and 62

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula7/Ex61.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula7/Ex61.py
@@ -3,19 +3,22 @@
 teclado. No final mostre a matriz com a
 formatação adequada.
 """
+# num = int(input(f"Digite o valor para a posição [{i+1}, {j+1}]: "))
 
 from random import randint
-list = []
+
+matriz = []
 
 for i in range(0,3,1):
-    line = []
+    linha = [] # Resets to empty on each iteration
     for j in range(0,3,1):
         num = randint(0, 10)
-        print(f"Linha: {i+1}º Coluna: {j+1}º. Valor inserido: ({num})")
-        line.append(num)
-    list.append(line)
+        print(f"Linha: {i+1}º Coluna: {j+1}º. Valor inserido: ({num})") 
+        linha.append(num)
+    matriz.append(linha)
 
+print("\nMatriz 3x3 formatada:")
 for i in range(0,3,1):
     for j in range(0,3,1):
-        print(f"{list[i][j]}", end=" ")
-    print()  # nova linha após cada linha da matriz
+        print(f"{matriz[i][j]:>2}",end=" ")
+    print()
--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula7/Ex63.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula7/Ex63.py
@@ -7,43 +7,42 @@ de 1 a 50 [sem repetir] e 2 estrelas de
 ser guardado numa lista composta.
 """
 
+# num = int(input(f"Digite o valor para a posição [{i+1}, {j+1}]: "))
 
 from random import randint
-list = []
-listValuesSecond = []
-sum = 0
 
+matriz = []
+sumEven = 0
+sumSecondColumn = 0
+
+# Matrix Constructor
 for i in range(0,3,1):
-    line = []
+    linha = [] # Resets to empty on each iteration
     for j in range(0,3,1):
         num = randint(0, 10)
-        print(f"Linha: {i+1}º Coluna: {j+1}º. Valor inserido: ({num})")
-        line.append(num)
-    list.append(line)
+        print(f"Linha: {i+1}º Coluna: {j+1}º. Valor inserido: ({num})") 
+        linha.append(num)
+    matriz.append(linha)
 
-# Print Matrix
+# Matrix B)
+print("\nMatriz 3x3 formatada:")
 for i in range(0,3,1):
     for j in range(0,3,1):
-        print(f"{list[i][j]}", end=" ")
-    print()  # nova linha após cada linha da matriz
+        print(f"{matriz[i][j]:>2}",end=" ")
+    print()
 
-# Sum Pares
 for i in range(0,3,1):
     for j in range(0,3,1):
-      sum += list[i][j]
+        # Sum of even numbers
+        if (matriz[i][j])%2 == 0:
+            sumEven += (matriz[i][j])
+        # Sum second column values
+        if j == 2:
+            sumSecondColumn += (matriz[i][j-1]) # info: Because Column 2 == index 1
+        # Maior valor da terceira linha
+        if i == 2:
+            maior = max(matriz[i])
 
-    print() 
-    
-# Sum Values da segunda Coluna
-for i in range(0,3,1):
-    for j in range(0,3,1):
-        print(f"")
-    print() 
-    
-# Maior valor da terceira linha
-for i in range(0,3,1):
-    for j in range(0,3,1):
-        print(f"")
-    print() 
-    
-print(f"A soma é: {sum}")
+print(f"A soma de todos os valores pares: {sumEven}")
+print(f"A soma dos valores da segunda coluna: {sumSecondColumn}")
+print(f"O maior valor da terceira linha.: {maior}")
