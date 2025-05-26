072067677e16f881210c260476a00380e104e0b7
João Mendes <mendes19966@gmail.com>
2025-05-26 12:29:04 +0100

    Update Ex66.py

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex66.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex66.py
@@ -9,7 +9,7 @@ for i in range(0,numJogadores,1):
   jogador["Roll"]= randint(1, 20)
   jogadores.append(jogador)
 
-for i in jogadores:
-  for key, value in jogador.items():
-    print(f"O jogador: {jogadores[i][key]}, rolou: {jogadores[i][value]}")
-  
+for jogador in jogadores:
+    print(f"O jogador: {jogador['Nome']}, rolou: {jogador['Roll']}")
+    
+    # Falta ordenar o valor dos rolls.
