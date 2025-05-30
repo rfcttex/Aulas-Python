d253a7b153f7309e91779ac4a081d53595330d0e
João Mendes <mendes19966@gmail.com>
2025-05-30 12:19:31 +0100

    Update Ex79.py

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex79.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex79.py
@@ -1,19 +1,17 @@
-# Crie um programa com uma função que vai
-# funcionar como a função input(), no
-# entanto vai fazer a validação para
-# aceitar apenas um valor numérico.
+def input_ultra(number):
+  """
+  Imitates the intput() function
+  :param number: String to be shown
+  :return: the number read
+  """
+  while True:
+    user_input = input(number)
+    if user_input.isdigit():
+        return int(user_input)
+    elif user_input.isnumeric():
+        return int(user_input)
+    else:
+      print("Erro! Input não válido. Digite um valor numérico.")
 
-def input_ultra(value):
-  if value == type(int):
-    returnValue = int(input(value))
-    print(value)
-  if value == type(float):
-    returnValue = float(input(value))
-    print(value)
-  elif value == type(str):
-    returnValue = "Erro! Input não válido." 
-    print(value)
-  return returnValue
-
-value = input("Digite um valor numérico: ")
-input_ultra(value)
+valor = input_ultra("Digite um valor numérico: ")
+print(f"Você digitou o valor numérico: {valor}")
