8717f6ceabefb7f8421e790660c77a41fc9532b1
João Mendes <mendes19966@gmail.com>
2025-05-31 22:06:07 +0100

    a

--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex81.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex81.py
@@ -10,3 +10,32 @@
 # ● IMC entre 30,0 e 34,9: Obesidade grau 1
 # ● IMC entre 35,0 e 39,9: Obesidade grau 2
 # ● IMC acima de 40,0: Obesidade grau 3 (obesidade mórbida)
+
+# Solicitar ao utilizador a altura e o peso
+altura = float(input("Digite a sua altura em metros (Utilize um ponto (.) Ex: 1.70): "))
+peso = float(input("Digite o seu peso em kg: "))
+
+def imc(altura,peso):
+  # Validar os valores de entrada
+  if altura <= 0 or peso <= 0:
+    print("Altura e peso devem ser maiores que zero.")
+  else:
+  # Calcular o IMC
+    imc = peso / (altura ** 2) # (**) = operador de expoencia.
+    # Determinar a classificação do IMC
+    if imc < 18.5:
+      classificacao = "Abaixo do peso"
+    elif 18.5 <= imc <= 24.9:
+      classificacao = "Peso normal"
+    elif 25.0 <= imc <= 29.9:
+      classificacao = "Sobrepeso"
+    elif 30.0 <= imc <= 34.9:
+      classificacao = "Obesidade grau 1"
+    elif 35.0 <= imc <= 39.9:
+      classificacao = "Obesidade grau 2"
+    else:
+      classificacao = "Obesidade grau 3 (obesidade mórbida)"
+    # Exibir o resultado
+    print(f"Seu IMC é {imc:.2f}. Classificação: {classificacao}.")
+    
+imc(altura,peso)
--- Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex82.py
+++ Aulas Fundamentos Python/Ricardo/Exercícios/Aula9/Ex82.py
@@ -3,3 +3,17 @@
 # deve digitar o comando e o sistema deve
 # retornar o manual. Quando o utilizador
 # digitar “FIM” o programa deve encerrar.
+
+msg = input("Qual mensagem de ajuda?: ").strip().lower()
+
+def helper(msg):
+  while True:
+    if msg == "fim":
+      print("Final!")
+      break
+    else:
+      print("Aqui está!\n")
+      help(msg)
+      msg = input("\nDeseja repetir? Sim-Insira a nova função \ (Não - escreva(fim): ").strip().lower()
+
+helper(msg)
