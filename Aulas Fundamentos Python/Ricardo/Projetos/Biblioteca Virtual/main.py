d2083cda24a6380c8820e2fa97c919499659b36e
João Mendes <mendes19966@gmail.com>
2025-06-04 22:38:49 +0100

    melhorias no menu principal e funções utilitárias
    - Adicionado loop principal ao menu para permitir múltiplas operações até o utilizador decidir sair.
    - Corrigida a navegação do menu e tratamento de opções inválidas.
    - Refatorada a função show_all_books para exibir corretamente os livros do catálogo importado não apenas o título de cada livro.
    - Estruturado o esqueleto para futuras funções de pesquisa, empréstimo e devolução de livros.
    - Formulação e melhora do esqueleto da documentação e acrescentado comentários para maior clareza do fluxo do programa.

--- Aulas Fundamentos Python/Ricardo/Projetos/Biblioteca Virtual/main.py
+++ Aulas Fundamentos Python/Ricardo/Projetos/Biblioteca Virtual/main.py
@@ -1,10 +1,30 @@
 from catalogo import catalogo as livros
+from utils.funcoes import *
 
 def main():
   """Para já a main apenas mostra os nossos dados guardados no ficheiro importado acima
+  1 - Mostra a mensagem de boas vindas através da função welcome
+  2 - Mostra a mensagem de boas vindas através da função welcome
+
   """
-  print(f"Working...")
-  print(f"{livros}")
+  welcome()
+  while True:
+    opcao = menu()
+    if opcao is None:
+      break
+    # Switch case 
+    match opcao:
+      case 1:
+        show_all_books()
+      case 2:
+        searches()
+      case _:
+        print("Opção inválida!")
+        print()
+        #TODO Mudar este input de (s/n) para uma keypress (ENTER)
+    continuar = input("Deseja voltar ao menu? (s/n): ").strip().lower()
+    if continuar != "s":
+      break
 
 if __name__ == "__main__":
   main()
--- Aulas Fundamentos Python/Ricardo/Projetos/Biblioteca Virtual/utils/funcoes.py
+++ Aulas Fundamentos Python/Ricardo/Projetos/Biblioteca Virtual/utils/funcoes.py
@@ -1,18 +1,27 @@
-def title(msg):
-    print(f"---{msg}---")
+def welcome():
+    """_summary_
+    """
+    print()
+    print(f"Bem-vindo à Biblioteca Virtual!!!")
 
 def menu():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     print("\n--- Main Menu ---")
-    print("[ 1 ] – Show all books")
-    print("[ 2 ] – Search")
-    print("[ 3 ] – Borrow")
-    print("[ 4 ] – Return")
-    print("[ 5 ] – Exit")
+    print("[ 1 ] – Mostrar todos os livros")
+    print("[ 2 ] – Pesquisar livro")
+    print("[ 3 ] – Emprestar livro")
+    print("[ 4 ] – Devolver livro")
+    print("[ 5 ] – Sair")
     while True:
         try:
-            option = int(input("Escolha uma opção: "))
+            option = int(input("\nEscolha uma opção: "))
             if option == 5:
-                print("Exiting program...")
+                print("A sair...")
+                break
             if 1 <= option < 5:
                 return option
             else:
@@ -24,8 +33,17 @@ def menu():
             print("Insira um valor válido!")
 
 def show_all_books():
-    title("Show all books!")
-    pass
+    """_summary_
+    """
+    from catalogo import catalogo as livros
+    print(f"A mostrar todos os livros..")    
+    print()
+    for livro in livros:
+        # TODO: Se mudar o index acrescentar id+1
+        print(
+            f"[ID {livro['id']}] “{livro['titulo']}” – {livro['autor']} ({livro['ano']}) [{livro['genero']}] → {livro['estado']}"
+            + (f" a → {livro['emprestado_por']}" if livro["emprestado_por"] else "")
+        )
 
 def searches():
     # Aqui provavelmente teremos que armazenar a maneira desejada de procura numa opção numa variável e depois usar-la para especificar as opções:
