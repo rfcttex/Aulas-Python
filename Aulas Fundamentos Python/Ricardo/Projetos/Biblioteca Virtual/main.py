69426a0cec70d48d061554ff761fc992f488c177
João Mendes <mendes19966@gmail.com>
2025-06-05 09:39:06 +0100

    Alterado o main menu input
    - Mudado o input para regressar para o menu principal.

--- Aulas Fundamentos Python/Ricardo/Projetos/Biblioteca Virtual/main.py
+++ Aulas Fundamentos Python/Ricardo/Projetos/Biblioteca Virtual/main.py
@@ -5,7 +5,6 @@ def main():
   """Para já a main apenas mostra os nossos dados guardados no ficheiro importado acima
   1 - Mostra a mensagem de boas vindas através da função welcome
   2 - Mostra a mensagem de boas vindas através da função welcome
-
   """
   welcome()
   while True:
@@ -21,10 +20,9 @@ def main():
       case _:
         print("Opção inválida!")
         print()
-        #TODO Mudar este input de (s/n) para uma keypress (ENTER)
-    continuar = input("Deseja voltar ao menu? (s/n): ").strip().lower()
-    if continuar != "s":
-      break
+    resp = input("Deseja voltar ao menu?: (Pressione ENTER para continuar / Digite qualquer coisa para sair)").strip()
+    if resp:
+        break
 
 if __name__ == "__main__":
   main()
